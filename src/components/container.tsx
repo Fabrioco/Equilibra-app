29e3c14e246b4e574a102192e3a6a0f5b64d3edb
Fabrioco <fabriciooliveiralopes50@gmail.com>
2026-02-01 10:41:56 -0300

    feat: criei o container para uso global

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { ScrollView, View } from "react-native";
+import { ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Container({ children }: { children: React.ReactNode }) {
   return (
-    <ScrollView contentContainerStyle={{ flex: 1 }}>
-      <View className="flex-1 bg-neutral-50">{children}</View>
-    </ScrollView>
+    <SafeAreaView className="flex-1 bg-neutral-50">
+      <ScrollView contentContainerStyle={{ flex: 1 }}>{children}</ScrollView>
+    </SafeAreaView>
   );
 }
