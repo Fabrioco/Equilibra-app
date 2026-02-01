9327e03d27e04fc82bfd534f0a66307566abaf35
Fabrioco <fabriciooliveiralopes50@gmail.com>
2026-02-01 10:38:24 -0300

    feat: adicionei o Toast e o AuthProvider

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,22 @@
 import { Slot } from "expo-router";
+import "../styles/global.css";
+import { AuthProvider } from "../contexts/auth-context";
+import Toast from "react-native-toast-message";
+import { toastConfig } from "../styles/toast-config";
 
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <>
+      <AuthProvider>
+        <Slot />
+      </AuthProvider>
+      <Toast
+        autoHide={true}
+        position="top"
+        topOffset={60}
+        visibilityTime={3000}
+        config={toastConfig}
+      />
+    </>
+  );
 }
